35f8660d042d7caca9d83830847bfb051ac2340c
Stepan Sm <stepan_smirnoff@mail.ru>
2022-08-08 19:47:36 +0300

    ДЗ 3-я задача

--- sem01_DZ/Program.cs
+++ sem01_DZ/Program.cs
@@ -1,4 +1,4 @@
-﻿// 1. Написать программу, которая на входе принимает 2 числа и выдает (сравнивает) какое число больше и какое число меньше
+﻿// 1. Написать программу, которая на входе принимает 2 числа и выдает (сравнивает) какое число больше и какое число меньше.
 Console.Clear();
 int numberA = new Random().Next(10, 99); //1 2 3 4 5 ... 9;
 Console.WriteLine(numberA);
@@ -19,9 +19,44 @@ else
 {
     Console.WriteLine("цифры равны" + numberA);
 }
+
 // 2. Напишите программу, которая принимает на вход 3 числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine();
+int numA = new Random().Next(10, 99); //1 2 3 4 5 ... 9;
+Console.WriteLine(numA);
+int numB = new Random().Next(10, 99); //1 2 3 4 5 ... 9;
+Console.WriteLine(numB);
+int numC = new Random().Next(10, 99); //1 2 3 4 5 ... 9;
+Console.WriteLine(numC);
+
+int max = numA;
+
+if (numA > max) max = numA;
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
 // 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine();
+int A = new Random().Next(10, 99); 
+Console.WriteLine(A);
+
+if (A % 2 == 0)
+{
+    Console.WriteLine("число четное: " + A);
+}
+else
+{
+    Console.WriteLine("число не четное: " + A);
+}
+
 // 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
 
 // 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
 // Console.Clear();
