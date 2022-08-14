81efee5a8dec7f170d02562b848be9d6a3c981c9
Stepan Sm <stepan_smirnoff@mail.ru>
2022-08-14 19:05:07 +0300

    работа по ошибкам в дз

--- sem003/Program.cs
+++ sem003/Program.cs
@@ -1,1 +1,228 @@
-﻿
+﻿// Напишите программу, которая принимает на входе координаты точек (Х и Y) при чем не равные 0 и выдает номер четверти плоскости,
+// в которой находится эта точка.
+
+// Console.Clear();
+// Console.WriteLine("Вводим х");
+// int x = new Random().Next(-10,10); //-9 -8 -7 1 2 3 4 5 ... 9;
+// // int x = Convert.ToInt32(Console.WritLine());
+// Console.WriteLine(x);
+// Console.WriteLine("Вводим y");
+// int y = new Random().Next(-10,10); //-9 -8 -7 1 2 3 4 5 ... 9;
+// // int y = Convert.ToInt32(Console.WritLine());
+// Console.WriteLine(y);
+
+// if (x>0 && y>0)
+// {
+//  Console.WriteLine("точка приналежать 1-й четверти плоскости");
+// }
+// else if (x<0 && y>0)
+// {
+//  Console.WriteLine("точка приналежать 2-й четверти плоскости");
+// }
+// else if (x<0 && y<0)
+// {
+//  Console.WriteLine("точка приналежать 3-й четверти плоскости");
+// }
+// else if (x>0 && y<0)
+// {
+//  Console.WriteLine("точка приналежать 4-й четверти плоскости");
+// }
+// else
+// {
+//     Console.WriteLine("точка не приналежать никакой четверти плоскости");
+// }
+
+// варианты решения
+
+
+// if (x > 0)
+// {
+//     if (y > 0)
+//     {
+//     Console.WriteLine("точка приналежать 1-й четверти плоскости");
+//     }
+//     else
+//     {
+//         Console.WriteLine("точка  приналежать 4-й четверти плоскости");
+//     }
+// }
+
+// if (x < 0)
+// {
+//     if (y > 0)
+//     {
+//     Console.WriteLine("точка  приналежать 2-й четверти плоскости");
+//     }
+//     else
+//     {
+//     Console.WriteLine("точка  приналежать 3-й четверти плоскости");   
+//     } 
+// }
+
+// Console.Write("Введите координаты точки X: ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты точки Y: ");
+// int y = int.Parse(Console.ReadLine()!);
+// string mes = "";
+
+// switch (x, y)
+// {
+//     case (>0, >0): mes = "1"; break;
+//     case (>0, <0): mes = "4"; break;
+//     case (<0, >0): mes = "2"; break;
+//     case (<0, <0): mes = "3"; break;
+//     default : mes = "введите отличное от 0"; break;
+// }
+// Console.WriteLine(mes);
+
+
+
+
+Console.Clear();
+
+void PrintQuarter(int x, int y) // Метод определния четверти заданной точки
+{
+
+if (x>0 && y>0)
+{
+ Console.WriteLine("точка приналежать 1-й четверти плоскости");
+}
+else if (x<0 && y>0)
+{
+ Console.WriteLine("точка приналежать 2-й четверти плоскости");
+}
+else if (x<0 && y<0)
+{
+ Console.WriteLine("точка приналежать 3-й четверти плоскости");
+}
+else if (x>0 && y<0)
+{
+ Console.WriteLine("точка приналежать 4-й четверти плоскости");
+}
+else
+{
+    Console.WriteLine("точка не приналежать никакой четверти плоскости");
+}
+}
+
+Console.WriteLine("Вводим х");
+int x = new Random().Next(-10,10); //-9 -8 -7 1 2 3 4 5 ... 9;
+// int x = Convert.ToInt32(Console.WritLine());
+Console.WriteLine(x);
+Console.WriteLine("Вводим y");
+int y = new Random().Next(-10,10); //-9 -8 -7 1 2 3 4 5 ... 9;
+// int y = Convert.ToInt32(Console.WritLine());
+Console.WriteLine(y);
+PrintQuarter(x, y);
+
+// int printQuarter(int a, int b)
+// {
+//     if (a>0 && b>0)
+//     {
+//         return 1;
+//     }
+//     else if (a<0 && b>0)
+//     {
+//         return 2;
+//     }
+//     else if (a<0 && b<0)
+//     {
+//         return 3;
+//     }
+//     else if (a>0 && b<0)
+//     {
+//         return 4;
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+
+
+// Console.WriteLine ("Введите координаты x");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите координаты y");
+// int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Номер четверти - {printQuarter(x, y)}");
+
+// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Console.Write("Введите номер четверти: ");
+// int quarter=int.Parse(Console.ReadLine());
+
+// switch (quarter)
+// {
+//     case 1:
+//     {
+//         Console.WriteLine("x>0 y>0");
+//         break;
+//     }
+//     case 2:
+//     {
+//         Console.WriteLine("x<0 y>0");
+//         break;
+//     }
+//     case 3:
+//     {
+//         Console.WriteLine("x<0 y<0");
+//         break;
+//     }
+//     case 4:
+//     {
+//         Console.WriteLine("x>0 y<0");
+//         break;
+//     }
+    
+//     default:
+//     {
+//         Console.WriteLine("Введена неправильная четверть");
+//         break;
+//     }
+// }
+
+
+// 18 Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+// 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+//Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+
+// Console.Clear();
+// Console.Write("Введите X1: ");
+// int x1=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y1: ");
+// int y1=int.Parse(Console.ReadLine());
+// Console.Write("Введите X2: ");
+// int x2=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y2: ");
+// int y2=int.Parse(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2));
+
+// Console.WriteLine($"d={d:f2}");
+
+
+
+// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// ДЗ 
+
+//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9. 
+// 5 -> 1, 8, 27, 64, 125
--- sem02_DZ/Program.cs
+++ sem02_DZ/Program.cs
@@ -7,12 +7,47 @@ Console.Clear();
 int numb = new Random().Next(100, 999);
 Console.WriteLine(numb);
 
-int A = numb / 10;
-int B = A % 10;
-Console.WriteLine($"число {B}");
+int A = numb / 10 % 10;
+// int B = A % 10;
+Console.WriteLine($"число {A}");
 Console.WriteLine();
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+
+// другие решения коллег
+//1)
+// Console.WriteLine("Enter a three-digit number");
+// int usersNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (1000 > usersNumber && usersNumber > 99)  // проверка на то что число является 3-х значным
+// {
+//  Console.Write("The entered number's second digit is: ");
+//  Console.WriteLine(usersNumber / 10 % 10);
+// }
+// else 
+// {
+//  Console.WriteLine("Entered number isn't a three-digit number.");
+// }
+
+//2)
+// Console.Write("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sec = number / 10;
+// int sec2 = sec % 10;
+// if (number >= 100 && number <= 999)
+// {
+//      Console.WriteLine(sec2);
+// }
+// else if (number <= 0)
+// {
+//      Console.WriteLine(-sec2);
+// }
+// else
+// {
+//      Console.WriteLine("Введено не трехзначное число");
+// }
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
@@ -20,10 +55,8 @@ Console.WriteLine();
 
 Console.Write("введите число:");
 int numF = Convert.ToInt32(Console.ReadLine());
-
 // int numF = new Random().Next(1, 9999);
 // Console.WriteLine(numF);
-
 string number = Convert.ToString(numF);
 if (number.Length>=3)
 {
@@ -34,6 +67,37 @@ else
     Console.WriteLine("третьей цифры нет");
 }
 Console.WriteLine();
+
+// другие решения коллег
+//1)
+// Console.WriteLine("Enter some number: ");
+// int usersNumber = Convert.ToInt32(Console.ReadLine());
+// int cycle = 0;
+
+// while (cycle < usersNumber) {
+//     if (usersNumber > 999) 
+//     {
+//         usersNumber = usersNumber / 10;
+//     }
+//     cycle++;
+// }
+// if (usersNumber > 99 && usersNumber < 1000) 
+// {
+//     Console.Write("The third digit is: ");
+//     Console.WriteLine(usersNumber % 10);
+// }
+// else 
+// {
+//  Console.WriteLine("There's no third digit in your number.");
+// }
+
+//2)
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// char[] array = Math.Abs(num).ToString().ToCharArray();
+// Console.WriteLine(array.Length > 3 ? $"Третья цифра введенного числа: {array[2]}": "Третьей цифры нет");
+
+
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 // 6 -> да
@@ -47,11 +111,11 @@ string[] daysofweek = { "Понедельник", "Вторник", "Среда"
 
 if ((dayofweek) > 7)
 {
-        Console.WriteLine("Нет дня недели соответсвующей введенной цифре");
-        return;
+    Console.WriteLine("Нет дня недели соответсвующей введенной цифре");
+    //return;
 }
 
-if ((dayofweek) > 5)
+if ((dayofweek) > 5 && dayofweek < 8 )
 {
     Console.WriteLine(daysofweek[dayofweek - 1]);
     Console.WriteLine("выходной день");
@@ -61,3 +125,50 @@ else
     Console.WriteLine(daysofweek[dayofweek-1]);
     Console.WriteLine("Это рабочий день");
 }
+
+
+// другие решения коллег
+//1)
+// Console.Write("Работаем? ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number >= 1 && number <= 5)
+// {
+//     Console.WriteLine("Да");
+// }
+// else if (number <= 0 || number >= 8)
+// {
+//     Console.WriteLine("Не в нашей вселенной)");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+//2
+// Console.Write("Введите день недели ");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day < 1 || day > 7)
+// {
+//     Console.Write("Не верно введен день");
+// }
+// else if (day <= 2 || day <= 5)
+// {
+//     Console.Write("Не является выходным");
+// }
+// else
+// {
+//     Console.Write("Ура, сегодня выходной!");
+// }
+
+//3)
+// Console.WriteLine("введите число дня недели");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day > 5 && day < 8)
+// {
+//     Console.WriteLine("Выходной");
+// }
+// if (day <= 5)
+// {
+//     Console.WriteLine("Не выходной");
+
