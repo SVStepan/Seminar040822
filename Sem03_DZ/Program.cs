c63d47105717c9b7d2e1b715af8076cf7fbdca5a
Stepan Sm <stepan_smirnoff@mail.ru>
2022-08-14 21:37:14 +0300

    DZ часть 1

---
+++ Sem03_DZ/Program.cs
@@ -0,0 +1,73 @@
+﻿// ДЗ 
+
+//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+Console.Clear();
+
+Console.WriteLine("введите число");
+int P = Convert.ToInt32(Console.ReadLine());
+// int P = 25152;
+
+int K = 0;
+if (P > 9999 && 100000 < P)
+{ 
+    int P1 = P;
+    while (P1 > 0)  // (условие пока) (Условие продолжения)  оператор цикла
+    {
+        K = K * 10 + P1 % 10;
+        P1 = P1 / 10;
+    }
+    if (K == P)
+        Console.WriteLine("Число является палиндромом");
+    else
+        Console.WriteLine("Число НЕ является палиндромом");
+}
+else
+Console.WriteLine("число не является 5-и значным");
+
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine();
+
+Double rasst;
+int t1 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9; // Console.WriteLine(t1);
+int s1 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9; // Console.WriteLine(s1);
+int r1 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9; // Console.WriteLine(r1);
+Console.WriteLine($"координаты 1-й точки : {t1}, {s1}, {r1}");
+
+int t2 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9; // Console.WriteLine(t2);
+int s2 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9; // Console.WriteLine(s2);
+int r2 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9; // Console.WriteLine(r2);
+Console.WriteLine($"координаты 2-й точки : {t2}, {s2}, {r2}");
+
+rasst = Math.Sqrt(Math.Pow(t2 - t1, 2) + Math.Pow(s2 - s1, 2) + Math.Pow(r2 - r1, 2));
+Console.WriteLine($"Расстояние = {rasst:f3}");
+Console.ReadLine();
+
+
+
+// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9. 
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine();
+Console.WriteLine("вводится число K");
+int N1 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9;
+Console.WriteLine(N1);
+int i = 1;
+int rez = 0;
+if (N1 < 0)
+{
+    N1 = N1 * -1;
+}
+while (i <= N1)
+{
+    rez = i*i*i;
+    Console.Write($" {rez},");
+    i++;
+}
--- sem003/Program.cs
+++ sem003/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Напишите программу, которая принимает на входе координаты точек (Х и Y) при чем не равные 0 и выдает номер четверти плоскости,
 // в которой находится эта точка.
 
+// решение без написанного ниже метода
 // Console.Clear();
 // Console.WriteLine("Вводим х");
 // int x = new Random().Next(-10,10); //-9 -8 -7 1 2 3 4 5 ... 9;
@@ -32,9 +33,10 @@
 //     Console.WriteLine("точка не приналежать никакой четверти плоскости");
 // }
 
-// варианты решения
 
+// варианты решения коллег
 
+//1)
 // if (x > 0)
 // {
 //     if (y > 0)
@@ -59,6 +61,8 @@
 //     } 
 // }
 
+
+//2)  используя функцию switch
 // Console.Write("Введите координаты точки X: ");
 // int x = int.Parse(Console.ReadLine()!);
 // Console.Write("Введите координаты точки Y: ");
@@ -77,10 +81,10 @@
 
 
 
-
+// решение с ипользованием написанной функции (Метод)
 Console.Clear();
 
-void PrintQuarter(int x, int y) // Метод определния четверти заданной точки
+void PrintQuarter(int x, int y) // Написали Метод определния четверти заданной точки, который далее можно где нибудь использовать
 {
 
 if (x>0 && y>0)
@@ -115,7 +119,8 @@ int y = new Random().Next(-10,10); //-9 -8 -7 1 2 3 4 5 ... 9;
 Console.WriteLine(y);
 PrintQuarter(x, y);
 
-// int printQuarter(int a, int b)
+
+// int printQuarter(int a, int b)   // Метод
 // {
 //     if (a>0 && b>0)
 //     {
@@ -139,19 +144,23 @@ PrintQuarter(x, y);
 //     }
 // }
 
+// Console.WriteLine ("Введите координаты а");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите координаты b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Номер четверти : {printQuarter(a, b)}");
 
-// Console.WriteLine ("Введите координаты x");
-// int x = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine ("Введите координаты y");
-// int y = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine($"Номер четверти - {printQuarter(x, y)}");
+
+
 
 // Напишите программу, которая по заданному номеру четверти,
 // показывает диапазон возможных координат точек в этой четверти (x и y).
 
+
+// работа коллег
+//1)
 // Console.Write("Введите номер четверти: ");
 // int quarter=int.Parse(Console.ReadLine());
-
 // switch (quarter)
 // {
 //     case 1:
@@ -174,7 +183,6 @@ PrintQuarter(x, y);
 //         Console.WriteLine("x>0 y<0");
 //         break;
 //     }
-    
 //     default:
 //     {
 //         Console.WriteLine("Введена неправильная четверть");
@@ -184,16 +192,54 @@ PrintQuarter(x, y);
 
 
 // 18 Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.WriteLine();
+
+int numbA = new Random().Next(1,8); //1 2 3 4 
+Console.WriteLine("четверть : " + numbA );
+
+if (numbA == 1)
+{
+    Console.WriteLine("X > 0, Y > 0 ");
+}
+if (numbA == 2)
+{
+    Console.WriteLine("X < 0, Y > 0");
+}
+if (numbA == 3)
+{
+    Console.WriteLine("X < 0, Y < 0");
+}
+if (numbA == 4)
+{
+    Console.WriteLine("X > 0, Y < 0");
+}
+else
+Console.WriteLine("Такой четверти в 2-х мерной системе координат нет");
 
 
 // 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 // A (3,6); B (2,1) -> 5,09 
 // A (7,-5); B (1,-1) -> 7,21
 
+Console.WriteLine();
+Double ras1;
+int x1 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9;
+// Console.WriteLine(x1);
+int y1 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9;
+Console.WriteLine($"координаты 1-й точки : {x1}, {y1}");
+int x2 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9;
+// Console.WriteLine(x2);
+int y2 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9;
+Console.WriteLine($"координаты 2-й точки : {x2}, {y2}");
 
-//Напишите программу, которая принимает на вход координаты двух точек
-// и находит расстояние между ними в 2D пространстве.
+ras1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние = {ras1:f3}");
 
+Console.ReadLine();
+
+
+// работа коллег
+//1)
 // Console.Clear();
 // Console.Write("Введите X1: ");
 // int x1=int.Parse(Console.ReadLine());
@@ -203,9 +249,7 @@ PrintQuarter(x, y);
 // int x2=int.Parse(Console.ReadLine());
 // Console.Write("Введите Y2: ");
 // int y2=int.Parse(Console.ReadLine());
-
 // double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2));
-
 // Console.WriteLine($"d={d:f2}");
 
 
@@ -214,15 +258,19 @@ PrintQuarter(x, y);
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
 
-// ДЗ 
-
-//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 23432 -> да
-// 12821 -> да
-// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
-// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 4, 9. 
-// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine();
+Console.WriteLine("введите число K");
+int N1 = new Random().Next(-10, 10); //-9 -8 -7 1 2 3 4 5 ... 9;
+Console.WriteLine(N1);
+int i = 1;
+int rez = 0;
+if (N1 < 0)
+{
+    N1 = N1 * -1;
+}
+while (i <= N1)
+{
+    rez = i*i;
+    Console.Write($" {rez},");
+    i++;
+}
