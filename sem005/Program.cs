19454cae0a9b3c258e99bb7a938d4f0213db9f64
Stepan Sm <stepan_smirnoff@mail.ru>
2022-08-19 00:04:02 +0300

    Семинар 05

---
+++ sem005/Program.cs
@@ -0,0 +1,199 @@
+﻿// Семинар 05
+Console.Clear();
+
+int[] CreateRandomArray(int N, int start, int end)  // создание случайного массива из N элементов  со значениями от 0 до 10, которые далеее надо вводить
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+int[] CreateArray()  // создание  массива с N-м колчиством элементов и вводом в ручном режиме каждого значения элемента массива
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] RandomArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {   
+        Console.WriteLine($"Введите {i+1} элемент массива");  // поочередно вводятся руками все значения массива с N-м колчиством элементов
+        RandomArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)  // вывод массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов в массиве");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число случайно генерируемого диапазона");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myRandomArray = CreateRandomArray(num, min, max);
+ShowArray(myRandomArray);
+Console.WriteLine("-------");
+
+// int[] myArray = CreateArray(); создаем массив с N-м колчиством элементов и вводом в ручном режиме каждого значения элемента массива
+// ShowArray(myArray);
+
+// Решение Mаксима через цикл
+
+// Random num1 = new Random();
+
+// Console.Write("Input the length of the array: ");
+// int length = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter the beginning of the range: ");
+// int start = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter the end of the range: ");
+// int end = int.Parse(Console.ReadLine()!);
+// int[] array = new int[length];
+
+// for(int i = 0; i < length; i++)
+// {
+//     array[i] = num1.Next(start,end);
+//     Console.Write(array[i]+ " ");
+// }
+
+// Random rand = new Random();
+// int[] array = new int[8];
+// Console.Write("[");
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] = rand.Next(0, 11);
+//     Console.Write($"{array[i]}" + (i < array.Length - 1 ? ", " : ""));
+// }
+// Console.Write("]");
+// Гая, добавил пару строчек в твой код)
+
+
+
+
+// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+// CreateRandomArray метод выше
+// ShowArray  метод выше
+
+int[] myArray = CreateRandomArray(12, -9, 9);
+ShowArray(myArray);
+
+int sum_pos = 0;
+int sum_neg = 0;
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] > 0)
+        sum_pos += myArray[i];
+    else
+        sum_neg += myArray[i];
+}
+
+Console.WriteLine($"Сумма положительных: {sum_pos}. Сумма отрицательных: {sum_neg}");
+Console.WriteLine();
+
+
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+// CreateRandomArray метод выше
+
+int[] moyarray = CreateRandomArray(8,-10,11);
+Console.WriteLine(String.Join(",",moyarray));
+for (int i = 0; i < moyarray.Length; i++)
+{
+    moyarray[i]=-moyarray[i];
+}
+Console.WriteLine(String.Join(",",moyarray));
+
+
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// CreateRandomArray метод задачи выше
+// ShowArray  метод выше
+
+// Console.WriteLine("Введите количество элементов в массиве");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число случайно генерируемого диапазона");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение random в массиве");
+// int random = Convert.ToInt32(Console.ReadLine());
+
+// bool flag = false;
+// int [] array = CreateRandomArray(num, min, max);
+// ShowArray(array);
+
+// for (int i = 0; i < array.length; i++)
+// {
+//     if (array[i] == random)
+//     {
+//         flag = true;
+//     }
+// if (flag == true)
+// {
+//     Console.WriteLine($"Число присутсвует в массиве: " + random)
+    
+// }
+// else
+//     {
+//          Console.WriteLine("Не присутствует");
+//     }
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+
+int[] myArray1 = CreateRandomArray(123, 0, 200);
+ShowArray(myArray1);
+for (int i = 0; i < myArray1.Length; i++)
+{
+    if (myArray1[i] >= 10 && myArray1[i] <= 99)
+    {
+        Console.Write($"{myArray1[i]}"+ "|");
+    }
+    else
+    Console.Write("-");
+}
+
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+// int[] myArray = CreateRandomArray(12, -9, 9);
+// ShowArray(myArray);
+
+// int result = 0;
+// int a = myArray.Length -1;
+// for (int i = 0; i < myArray.Length/2 ; i++)
+// {
+//     result = myArray[i] * myArray[a];
+//     a -= 1;
+//     Console.Write($"[{String.Join(",", result)}]");
+// }
+// if(myArray.Length%2 != 0)
+// {
+//     Console.Write($"[{String.Join(",", myArray[myArray.Length/2])}]");
+// }
--- sem04_DZ/Program.cs
+++ sem04_DZ/Program.cs
@@ -5,16 +5,77 @@ Console.Clear();
 
 Console.Write("введите первое число а:");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите второе число b:");
+Console.Write("введите степень b:");
 int b = Convert.ToInt32(Console.ReadLine());
-int rez = a;
-for (int i = 1; i < b; i++)
+
+int rez = 0;
+rez = a;
+for (int  i = 1; i < b; i++)
     {
-        rez = rez * a;
+        rez = a * rez ;
     }
 
-Console.Write(rez);
+Console.WriteLine (rez);
 Console.WriteLine();
+
+
+// Console.WriteLine($"{a} в степень {b} = {Pow(a,b)}");   // решение через метод
+
+
+// int Pow(int num1, int step)
+// {
+//     if (b == 0) return 1;
+//     int rez = num1;
+// for (int i = 2; i <= step; i++)
+//     {
+//         //rez = rez * a;
+//         rez *= num1;
+//     }
+//     return rez;
+// }
+
+// Console.WriteLine();
+
+
+// 25  решение коллег
+
+// Console.WriteLine("Enter a number: ");
+// int numA = int.Parse(Console.ReadLine() ?? "0");
+// Console.WriteLine("And its power: ");
+// int numB = int.Parse(Console.ReadLine() ?? "0");
+// numB = Math.Abs(numB);
+// int firstA = numA;
+// int i = 0;
+
+//Console.WriteLine($"{numA} raised to a power {numB} = {Math.Pow(numA, numB)}");
+
+// if (numA == 0)
+// {
+//     Console.WriteLine($"{firstA} raised to a power {numB} = {numA}");    
+// }
+
+// else 
+// {
+//     while (i < numB) 
+//     {
+//         numA *= firstA;
+//         i++;
+//     }
+//     numA /= firstA;
+//     Console.WriteLine($"{firstA} raised to a power {numB} = {numA}");
+// }
+
+
+// Console.Write("Введите A: ");
+// int number1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите B: ");
+// int number2 = int.Parse(Console.ReadLine());
+
+// int num = Convert.ToInt32(Math.Pow(number1, number2));
+// Console.WriteLine(num);
+
+
+
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
@@ -33,6 +94,8 @@ while (num > 0)
 Console.WriteLine("Сумма = :");
 Console.WriteLine(summ);
 
+
+
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
 // 6, 1, 33 -> [6, 1, 33]
@@ -47,3 +110,29 @@ for ( int j = 0; j < 8; j++)
     Console.Write (array[j] + " ");
 }
 
+// //29
+
+// Random rand = new Random();
+// int [8] arr;
+// for(int i = 0; i < arr.Length; i++)
+// {
+//     arr[i] = rand.Next();
+//     Console.Write($" {arr[i]},");
+// }
+
+// решение преподователя по ДЗ
+
+Console.WriteLine();
+int[] arra = GetRandomArray(8,0,100);
+Console.WriteLine($"Массив из 8 элементов, [{String.Join(", ", arra)}]");
+
+int[] GetRandomArray(int size,int minValue,int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue,maxValue);
+    }
+
+    return result;
+}
